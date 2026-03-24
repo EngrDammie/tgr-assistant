38edbc80857feb0ce72d5dbe01c2fed4247271fb
Engr. Dammie <dammieoptimus@gmail.com>
2026-03-24 05:58:29 +0000

    Fix: Add CORS support for dashboard

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = requi
 const pino = require('pino');
 const QRCode = require('qrcode');
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const cron = require('node-cron');
@@ -18,6 +19,7 @@ const db = require('./database');
 const logger = require('./logger');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static('.'));
 
